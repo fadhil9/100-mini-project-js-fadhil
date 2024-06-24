ce8951553ca1cfda928004b163934627674918b2
fadhil9 <fadhilmuhamad272@gmail.com>
2024-06-24 22:38:02 +0700

    lanjut

---
+++ 3.ageCalculator/index.js
@@ -0,0 +1,12 @@
+// dom nya
+const button = document.getElementById('button');
+const tgl = document.getElementById('date');
+const output = document.getElementById('output');
+
+// function menghitung
+function calculateAge() {
+    
+}
+
+//function  input
+//btn addeven click aktifin function meeting
