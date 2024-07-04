bc4109a5e7218fb4c7804f594d2b4b0d976f43ea
fadhil9 <fadhilmuhamad272@gmail.com>
2024-07-04 14:39:39 +0700

    update

---
+++ 4.ImageSearchApp/repeat/index.js
@@ -0,0 +1,6 @@
+const acceskey = "RZEIOVfPhS7vMLkFdd2TSKGFBS4o9_FmcV1Nje3FSjw";
+
+const inputElement = document.getElementById('search-input');
+const enterElement = document.getElementById('enter');
+const showMoreElement = document.getElementById('show-more');
+
