bf0c88ca0340558475be0bd1b59b0059b7cb75ce
fadhil9 <fadhilmuhamad272@gmail.com>
2024-07-15 14:17:55 +0700

    update

--- 5.todolist/index.js
+++ 5.todolist/index.js
@@ -3,3 +3,30 @@ const formEl = document.querySelector(".form");
 const inputEl = document.querySelector(".input");
 
 const ulEl = document.querySelector(".list");
+
+
+let list = JSON.parse(localStorage.getItem('list'));
+
+if(list){
+    list.forEach(task => {
+        toDoList(task);
+    });
+}
+
+formEl.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    toDoList();
+})
+
+function toDoList(task) {
+    let newTask = inputEl.value;
+
+    if(task) {
+        newTask = task.name;
+    }
+
+    const liel = document.createElement("li");
+    if(task && task.checked) {
+        liel.classList.add("checked");
+    }
+}
