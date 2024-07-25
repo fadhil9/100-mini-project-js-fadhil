d01c5d9444cc533ec69778136b8748373676afa4
fadhil9 <fadhilmuhamad272@gmail.com>
2024-07-25 20:42:52 +0700

    update

--- 5.todolist/repeat/indexx.js
+++ 5.todolist/repeat/indexx.js
@@ -1,1 +1,11 @@
 const formEl = document.querySelector('.form');
+const inputEl = document.querySelector('.input');
+const ulEl = document.querySelector('.list');
+
+const dummyDataTodolist = [
+    {name:"belajar", checked:true},
+    {name:"jogging", checked:false}
+]
+//buat localstorage data array object nama tipe string dan checked tipe bolean untuk todolistnya
+localStorage.setItem("list",JSON.stringify(dummyDataTodolist));
+//buat if statement, kalo ada tampilin lewat function todolist
